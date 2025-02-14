a652adf600a0334374504a01d14af893a68c6e53
Jiyang Zhang <zjiyang@students.cs.ubc.ca>
2025-02-14 13:52:13 -0800

    add implementation for Genre class

--- src/main/model/Genre.java
+++ src/main/model/Genre.java
@@ -2,17 +2,26 @@ package model;
 
 //Represents genre of a movie 
 public class Genre {
-    
+    private String name;
 
     // EFFECTS: Construct a genre with given name.
     public Genre(String name) {
-        //stub
+        this.name = name;
     }
 
     public String getGenreName() {
-        return ""; //stub
+        return this.name;
     }
     
     // Define commonly used genres as public static final objects
+    public static final Genre Action = new Genre("Action");
+    public static final Genre Romance = new Genre("Romance");
+    public static final Genre Drama = new Genre("Drama");
+    public static final Genre Comedy = new Genre("Comedy");
+    public static final Genre Horror = new Genre("Horror");
+    public static final Genre Sci_Fi = new Genre("Sci-Fi");
+    public static final Genre Animation = new Genre("Animation");
+    public static final Genre Documentary = new Genre("Documentary");
 
 }
+
