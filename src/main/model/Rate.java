014d823f192e52a89a59dcc55c5e812846b09fbc
Jiyang Zhang <zjiyang@students.cs.ubc.ca>
2025-02-14 13:51:35 -0800

    added implementation for Rate class

--- src/main/model/Rate.java
+++ src/main/model/Rate.java
@@ -2,23 +2,37 @@ package model;
 
 // Represent a rate with score (0-5) from a userID 
 public class Rate {
+    int userID;
+    int score;
 
     // REQUIRES: userID > 0; 0 <= score <= 5, 
     // EFFECTS: contruct a score with given userID and score.
     public Rate(int userID, int score) {
+        this.userID = userID;
+        this.score = score;
     }
 
     // below are getters
     public int getUserID() {
-        return 1;
+        return this.userID;
     }
 
     public int getScore() {
-        return 1;
-
+        return this.score;
     }
 
     // Define 5 Rate for userID 001, as public static final objects for test
-
+    public static final Rate rate0 = new Rate(001,0);
+    public static final Rate rate1 = new Rate(001,1);
+    public static final Rate rate2 = new Rate(001,2);
+    public static final Rate rate3 = new Rate(001,3);
+    public static final Rate rate4 = new Rate(001,4);
+    public static final Rate rate5 = new Rate(001,5);
+    public static final Rate rate6 = new Rate(001,6);
+    public static final Rate rate7 = new Rate(001,7);
+    public static final Rate rate8 = new Rate(001,8);
+    public static final Rate rate9 = new Rate(001,9);
+    public static final Rate rate10 = new Rate(001,10);
 
 }
+
