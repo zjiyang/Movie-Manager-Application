488c9ac9638a106553f480e8a91d92f3d1520cc6
Jiyang Zhang <zjiyang@students.cs.ubc.ca>
2025-02-14 13:50:53 -0800

    Added implementation for StreamService class

--- src/main/model/StreamService.java
+++ src/main/model/StreamService.java
@@ -2,16 +2,21 @@ package model;
 
 //Represents Stream Platform for watch movie online
 public class StreamService {
+    private String name;
 
     // EFFECTS: Construct a movie stream service provider with given name.
     public StreamService(String name) {
-
+        this.name = name;
     }
 
     public String getStreamPlatformName() {
-        return "";
+        return this.name;
     }
 
     //Define commonly used StreamService as public static final objects
+    public static final StreamService Netflix = new StreamService("Netflix");
+    public static final StreamService DisneyPlus = new StreamService("DisneyPlus");
+    public static final StreamService AppleTV = new StreamService("AppleTV");
+    public static final StreamService PrimeVideo = new StreamService("PrimeVideo");
 
 }
