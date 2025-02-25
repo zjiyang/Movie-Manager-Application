911a6059eb88b00e5eca196202d57fddd25bf792
Jiyang Zhang <zjiyang@students.cs.ubc.ca>
2025-02-24 20:52:29 -0800

    Update specification for JsonReader class

--- src/main/persistence/JsonReader.java
+++ src/main/persistence/JsonReader.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 
 import org.json.*;
 
-// Represents a reader that reads workroom from JSON data stored in file
+// Represents a reader that reads MovieDatabase from JSON data stored in file
 public class JsonReader {
     private String source;
 
@@ -14,7 +14,7 @@ public class JsonReader {
     public JsonReader(String source) {
     }
 
-    // EFFECTS: reads workroom from file and returns it;
+    // EFFECTS: reads MovieDatabase from file and returns it;
     // throws IOException if an error occurs reading data from file
     public MovieDataBase read() throws IOException {
         return null; //stub
@@ -25,18 +25,18 @@ public class JsonReader {
         return ""; //stub
     }
 
-    // EFFECTS: parses workroom from JSON object and returns it
+    // EFFECTS: parses MovieDatabase from JSON object and returns it
     private MovieDataBase parseMovieDataBase(JSONObject jsonObject) {
         return null; //stub
     }
 
     // MODIFIES: wr
-    // EFFECTS: parses Movies from JSON object and adds them to database
+    // EFFECTS: parses Movies from JSON object and adds them to MovieDatabase
     private void addMovies(MovieDataBase database, JSONObject jsonObject) {
     }
 
     // MODIFIES: movies
-    // EFFECTS: parses Movie from JSON object and adds it to database
+    // EFFECTS: parses Movie from JSON object and adds it to MovieDatabase
     private void addMovie(MovieDataBase database, JSONObject jsonObject) {
     }
 }
