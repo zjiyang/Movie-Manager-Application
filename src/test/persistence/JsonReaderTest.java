41c106817d38db20b159776907d586861bee9036
Jiyang Zhang <zjiyang@students.cs.ubc.ca>
2025-02-26 16:04:09 -0800

    Added test for JsonReaderTest

--- src/test/persistence/JsonReaderTest.java
+++ src/test/persistence/JsonReaderTest.java
@@ -1,8 +1,12 @@
 package persistence;
 
-import model.*;
-import persistence.*;
 import org.junit.jupiter.api.Test;
+
+import model.Genre;
+import model.Movie;
+import model.MovieDataBase;
+import model.Rate;
+import model.StreamService;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -10,43 +14,36 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class JsonWriterTest extends JsonTest {
-    //NOTE TO CPSC 210 STUDENTS: the strategy in designing tests for the JsonWriter is to
-    //write data to a file and then use the reader to read it back in and check that we
-    //read in a copy of what was written out.
+class JsonReaderTest extends JsonTest {
 
     @Test
-    void testWriterInvalidFile() {
+    void testReaderNonExistentFile() {
+        JsonReader reader = new JsonReader("./data/noSuchFile.json");
         try {
-            MovieDataBase database = new MovieDataBase();
-            JsonWriter writer = new JsonWriter("./data/my\0illegal:fileName.json");
-            writer.open();
-            fail("IOException was expected");
+            MovieDataBase dataBase = reader.read();
+            fail("IOException expected");
         } catch (IOException e) {
             // pass
         }
     }
 
     @Test
-    void testWriterEmptyWorkroom() {
+    void testReaderEmptyWorkRoom() {
+        JsonReader reader = new JsonReader("./data/testEmptyMovieDataBase.json");
         try {
-            MovieDataBase database = new MovieDataBase();
-            JsonWriter writer = new JsonWriter("./data/testEmptyMovieDataBase.json");
-            writer.open();
-            writer.write(database);
-            writer.close();
-
-            JsonReader reader = new JsonReader("./data/testEmptyMovieDataBase.json");
-            database = reader.read();
+            MovieDataBase dataBase = reader.read();
+            assertTrue(dataBase.getDataBase().isEmpty());
         } catch (IOException e) {
-            fail("Exception should not have been thrown");
+            fail("Couldn't read from file");
         }
     }
 
     @Test
-    void testWriterGeneralWorkroom() {
+    void testReaderGeneralWorkRoom() {
+        JsonReader reader = new JsonReader("./data/testGeneralMovieDataBase.json");
         try {
-            MovieDataBase database = new MovieDataBase();
+            MovieDataBase dataBase = reader.read();
+            ArrayList<Movie> movies = dataBase.getDataBase();
 
             Rate testRate1 = new Rate(00001, 5);
             Rate testRate2 = new Rate(00001, 6);
@@ -64,19 +61,6 @@ class JsonWriterTest extends JsonTest {
             m2.rateMovie(testRate2);
             m2.addStreamService(StreamService.AppleTV);
 
-            database.addMovie(m1);
-            database.addMovie(m2);
-
-            JsonWriter writer = new JsonWriter("./data/testWriterGeneralMovieDataBase.json");
-            writer.open();
-            writer.write(database);
-            writer.close();
-
-            JsonReader reader = new JsonReader("./data/testWriterGeneralMovieDataBase.json");
-            database = reader.read();
-            ArrayList<Movie> movies;
-            movies = database.getDataBase();
-
             ArrayList<Genre> testGenres1 = new ArrayList<Genre>();
             testGenres1.add(Genre.Drama);
             ArrayList<Genre> testGenres2 = new ArrayList<Genre>();
@@ -93,12 +77,15 @@ class JsonWriterTest extends JsonTest {
             ArrayList<StreamService> testSS2 = new ArrayList<StreamService>();
             testSS1.add(StreamService.AppleTV);
 
+            dataBase.addMovie(m1);
+            dataBase.addMovie(m2);
+
             assertEquals(2, movies.size());
             checkMovie(movies.get(0), "m1", 2024, testGenres1, testRates1, testSS1);
             checkMovie(movies.get(1), "m2", 2025, testGenres2, testRates2, testSS2);
 
         } catch (IOException e) {
-            fail("Exception should not have been thrown");
+            fail("Couldn't read from file");
         }
     }
 }
